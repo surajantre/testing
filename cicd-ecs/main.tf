d5a190dab51cabc748822a601f29a0e7d5783478
SURAJ ANTRE <120030250+surajantre@users.noreply.github.com>
2025-08-21 05:13:05 +0530

    Update main.tf

--- cicd-ecs/main.tf
+++ cicd-ecs/main.tf
@@ -1,6 +1,12 @@
 provider "aws" {
-  region = "ap-south-1"
+  region                  = "ap-south-1"
+  access_key              = var.aws_access_key
+  secret_key              = var.aws_secret_key
 }
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 
 resource "aws_ecs_cluster" "my_cluster" {
   name = "my-ecs-cluster"
