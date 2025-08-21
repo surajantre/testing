48903dfbe98c5f306401890027fc91aff9340715
SURAJ ANTRE <120030250+surajantre@users.noreply.github.com>
2025-08-21 06:00:09 +0530

    Update main.tf

--- cicd-ecs/main.tf
+++ cicd-ecs/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region                  = "ap-south-1"
-  access_key              = var.aws_access_key
-  secret_key              = var.aws_secret_key
+  region     = "ap-south-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 variable "aws_access_key" {}
@@ -11,52 +11,26 @@ resource "aws_ecs_cluster" "my_cluster" {
   name = "my-ecs-cluster"
 }
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Effect = "Allow"
-        Sid    = ""
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_policy_attach" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 resource "aws_ecs_task_definition" "my_task" {
   family                   = "my-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  container_definitions = jsonencode([
-    {
-      name      = "my-container"
-      image     = "072152842782.dkr.ecr.ap-south-1.amazonaws.com/my-app:latest"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
-      ]
-    }
-  ])
+  # ✅ Reference existing manually created role
+  execution_role_arn = "arn:aws:iam::072152842782:role/ecsTaskExecutionRole"
+
+  container_definitions = jsonencode([{
+    name      = "my-container"
+    image     = "072152842782.dkr.ecr.ap-south-1.amazonaws.com/my-app:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
 }
-
 
 resource "aws_ecs_service" "my_service" {
   name            = "my-service"
@@ -66,8 +40,8 @@ resource "aws_ecs_service" "my_service" {
   desired_count   = 1
 
   network_configuration {
-    subnets         = ["subnet-06659f5fb76f972d1"]  # Replace
-    security_groups = ["sg-0f1e96c240bc26eba"]      # Replace
+    subnets          = ["subnet-06659f5fb76f972d1"]
+    security_groups  = ["sg-0f1e96c240bc26eba"]
     assign_public_ip = true
   }
 }
